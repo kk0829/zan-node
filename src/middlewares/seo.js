46a656c9c6c18fd0c89abbc9c713120d14dffd3a
kk <kk@youzan.com>
2017-10-24 16:52:02 +0800

    feat: SEO 中间件支持正则表达式、通配符

--- src/middlewares/seo.js
+++ src/middlewares/seo.js
@@ -1,19 +1,55 @@
 import UA from 'zan-ua';
+import toRegexp from 'path-to-regexp';
 
-module.exports = function (options) {
-    let SEO_CONSTANTS = require(options.path);
+module.exports = function(options) {
+    const SEO_CONSTANTS = require(options.path);
+    let config = [];
+    if (Array.isArray(SEO_CONSTANTS)) {
+        for (let i = 0; i < SEO_CONSTANTS.length; i++) {
+            config.push({
+                key: SEO_CONSTANTS[i].key,
+                value: SEO_CONSTANTS[i].value,
+                reg: toRegexp(SEO_CONSTANTS[i].key)
+            });
+        }
+    } else {
+        for (let item in SEO_CONSTANTS) {
+            config.push({
+                key: item,
+                reg: toRegexp(item),
+                value: SEO_CONSTANTS[item]
+            });
+        }
+    }
+    let defaultItem = config.find((item) => {
+        return item.key === 'default';
+    }) || {};
+    let wapDefault = config.find((item) => {
+        return item.key === 'wap_default';
+    }) || {};
+    let wwwDefault = config.find((item) => {
+        return item.key === 'www_default';
+    }) || {};
 
     return async(ctx, next) => {
-        let isMobile = UA.isMobile(ctx.headers['user-agent']);
-        let obj = Object.assign(
-            SEO_CONSTANTS.default,
-            isMobile ? SEO_CONSTANTS.wap_default || {} : SEO_CONSTANTS.www_default || {},
-            SEO_CONSTANTS[ctx.path] || {}
+        const isMobile = UA.isMobile(ctx.headers['user-agent']);
+        let resultObj = Object.assign({},
+            defaultItem.value,
+            isMobile ? wapDefault.value : wwwDefault.value
         );
+        let matchItem;
+        for (let i = 0; i < config.length; i++) {
+            if (config[i].reg.test(ctx.path)) {
+                matchItem = config[i].value;
+                break;
+            }
+        }
+        resultObj = Object.assign(resultObj, matchItem || {});
+
         ctx.setState({
-            title: obj.title + obj.title_suffix,
-            keywords: obj.keywords,
-            description: obj.description
+            title: resultObj.title + resultObj.title_suffix,
+            keywords: resultObj.keywords,
+            description: resultObj.description
         });
         await next();
     };
