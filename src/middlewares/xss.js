736545bf62341b09814c871c8b17bfca23c85492
kk <kk@youzan.com>
2017-11-06 18:02:18 +0800

    feat: WHITELISTS 添加close 参数

--- src/middlewares/xss.js
+++ src/middlewares/xss.js
@@ -21,6 +21,11 @@ module.exports = function(options) {
         let query = ctx.query;
         let bodyData = ctx.request.body;
         let one = options.WHITELISTS.find((item) => item.pathReg.test(ctx.path));
+        // 黑科技
+        if (one && one.options && one.options.close) {
+            await next();
+            return;
+        }
         let wrapOptions = one ? one.options : {};
         const whiteList = xss.getDefaultWhiteList();
 
