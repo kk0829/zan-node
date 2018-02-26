d9c2f18e9bcc9692f67c841093a2665d1eb908bf
kk <kk@youzan.com>
2018-02-26 15:34:21 +0800

    feat: 更新 context 扩展方式

--- src/extend/context.js
+++ src/extend/context.js
@@ -5,172 +5,187 @@ const querystring = require('querystring');
 const context = require('koa/lib/context');
 const mapKeysToSnakeCase = require('zan-utils/string/mapKeysToSnakeCase');
 const mapKeysToCamelCase = require('zan-utils/string/mapKeysToCamelCase');
+const util = require('../lib/util');
 
 /**
  * context 扩展方法
  */
 
-context.getConfig = function getConfig(name) {
+function transformData(key, value, toSnakeCase) {
+  const plainObject = isPlainObject(key);
+  let data = {};
+  if (plainObject) {
+    data = key;
+  } else {
+    data[key] = value;
+  }
+  toSnakeCase = (plainObject ? value : toSnakeCase) || false;
+  if (toSnakeCase) {
+    data = mapKeysToSnakeCase(data);
+  }
+  return data;
+}
+
+const proto = {
+
+  get acceptJSON() {
+    if (this.path.endsWith('.json')) {
+      return true
+    } else {
+      return false;
+    }
+  },
+
+  hasState(key) {
+    return key ? this.state.hasOwnProperty(key) : false;
+  },
+
+  /**
+   * @method getState
+   * @param  {String/null} key
+   * @param  {Boolean} toCamelCase 是否转成驼峰格式
+   */
+  getState(key, toCamelCase = false) {
+    let data = key ? this.state[key] : this.state;
+    return toCamelCase ? mapKeysToCamelCase(data) : data;
+  },
+
+  setState(key, value, toSnakeCase = false) {
+    const data = transformData(key, value, toSnakeCase);
+    this.state = Object.assign(this.state || {}, data);
+  },
+
+  setEnv(key, value, toSnakeCase = false) {
+    const data = transformData(key, value, toSnakeCase);
+    this.state = Object.assign(this.state || {}, data);
+    this.state.global = Object.assign(this.state.global || {}, data);
+  },
+
+  setGlobal(key, value, toSnakeCase = false) {
+    const data = transformData(key, value, toSnakeCase);
+    this.state.global = Object.assign(this.state.global || {}, data);
+  },
+
+  getGlobal(key, toCamelCase = false) {
+    let data = key ? this.state.global[key] : this.state.global;
+    return toCamelCase ? mapKeysToCamelCase(data) : data;
+  },
+
+  // 如果查询参数和请求体参数都有这个字段，则请求体中的参数优先级更高
+  getRequestData(key) {
+    const data = defaultsDeep({}, this.request.body, this.query);
+    return key ? data[key] : data;
+  },
+
+  getPostData(key) {
+    const data = this.request.body;
+    return key ? data[key] : data;
+  },
+
+  pushBeforeRender(fn) {
+    if (typeof fn === 'function') this.beforeRenderFns.push(fn);
+  },
+
+  getConfig(name) {
     if (!name) {
-        return this.app.projectConfig;
+      return this.app.projectConfig;
     }
     let arr = name.split('.');
     let result = this.app.projectConfig;
     let i = 0;
     while (arr[i]) {
-        result = result[arr[i]];
-        i++;
+      result = result[arr[i]];
+      i++;
     }
     return result;
-};
+  },
 
-context.pushBeforeRender = function pushBeforeRender(fn) {
-    if (typeof fn === 'function') this.beforeRenderFns.push(fn);
-};
-
-context.hasState = function hasState(key) {
-    return key ? this.state.hasOwnProperty(key) : false;
-};
-
-function transformData(key, value, toSnakeCase) {
-    const plainObject = isPlainObject(key);
-    let data = {};
-    if (plainObject) {
-        data = key;
-    } else {
-        data[key] = value;
-    }
-    toSnakeCase = (plainObject ? value : toSnakeCase) || false;
-    if (toSnakeCase) {
-        data = mapKeysToSnakeCase(data);
-    }
-    return data;
-}
-
-/**
- * @method getState
- * @param  {String/null} key
- * @param  {Boolean} toCamelCase 是否转成驼峰格式
- */
-context.getState = function getState(key, toCamelCase = false) {
-    let data = key ? this.state[key] : this.state;
-    return toCamelCase ? mapKeysToCamelCase(data) : data;
-};
-
-context.setState = function setState(key, value, toSnakeCase = false) {
-    const data = transformData(key, value, toSnakeCase);
-    this.state = Object.assign(this.state || {}, data);
-};
-
-context.setEnv = function setEnv(key, value, toSnakeCase = false) {
-    const data = transformData(key, value, toSnakeCase);
-    this.state = Object.assign(this.state || {}, data);
-    this.state.global = Object.assign(this.state.global || {}, data);
-};
-
-context.setGlobal = function setGlobal(key, value, toSnakeCase = false) {
-    const data = transformData(key, value, toSnakeCase);
-    this.state.global = Object.assign(this.state.global || {}, data);
-};
-
-context.getGlobal = function getGlobal(key, toCamelCase = false) {
-    let data = key ? this.state.global[key] : this.state.global;
-    return toCamelCase ? mapKeysToCamelCase(data) : data;
-};
-
-// 如果查询参数和请求体参数都有这个字段，则请求体中的参数优先级更高
-context.getRequestData = function getRequestData(key) {
-    const data = defaultsDeep({}, this.request.body, this.query);
-    return key ? data[key] : data;
-};
-
-context.getPostData = function getPostData(key) {
-    const data = this.request.body;
-    return key ? data[key] : data;
-};
-
-context.getQueryData = function getQueryData(key) {
+  getQueryData(key) {
     const str = this.querystring;
     let parsed = querystring.parse(str);
     Object.keys(parsed).forEach((item) => {
-        if (Array.isArray(parsed[item]) && union(parsed[item]).length === 1) {
-            parsed[item] = parsed[item][0];
-        }
+      if (Array.isArray(parsed[item]) && union(parsed[item]).length === 1) {
+        parsed[item] = parsed[item][0];
+      }
     });
     return key ? parsed[key] : parsed;
-};
+  },
 
-context.getRawCookies = function getRawCookies() {
+  getRawCookies() {
     return this.headers.cookie;
-};
+  },
 
-context.getCookies = function getCookies() {
+  getCookies() {
     let result = {};
     let cookies = this.headers.cookie.split('; ');
     for (let i = 0; i < cookies.length; i++) {
-        let arr = cookies[i].split('=');
-        result[arr[0]] = arr[1];
+      let arr = cookies[i].split('=');
+      result[arr[0]] = arr[1];
     }
     return result;
-};
+  },
 
-context.getCookie = function(name, options) {
+  getCookie(name, options) {
     return this.cookies.get(name, options);
-};
+  },
 
-context.setCookie = function(name, value, options) {
+  setCookie(name, value, options) {
     return this.cookies.set(name, value, options);
-};
+  },
 
-context.throwBusinessError = function(type, code, msg) {
+  throwBusinessError(type, code, msg) {
     this.businessErrorType = type;
     this.businessErrorContent = {
-        code,
-        msg
+      code,
+      msg
     };
-};
+  },
 
-context.json = function(status, data, extra) {
+  json(status, data, extra) {
     if (extra) { // 三个参数情况，这3个参数分别为 code msg data
-        this.body = {
-            code: status,
-            msg: data,
-            data: extra
-        };
+      this.body = {
+        code: status,
+        msg: data,
+        data: extra
+      };
     } else {
-        this.body = {
-            code: status.code,
-            msg: status.msg,
-            data
-        };
+      this.body = {
+        code: status.code,
+        msg: status.msg,
+        data
+      };
     }
     return;
-};
+  },
 
-context.r = function(code, msg, data) {
+  r(code, msg, data) {
     this.body = {
-        code,
-        msg,
-        data: mapKeysToSnakeCase(data)
+      code,
+      msg,
+      data: mapKeysToSnakeCase(data)
     };
     return;
-};
+  },
 
-context.getSession = function(key) {
+  getSession(key) {
     return key ? this.sessionCache[key] : this.sessionCache;
-};
+  },
 
-context.setSession = function(key, value) {
+  setSession(key, value) {
     let plainObject = isPlainObject(key);
     let data = {};
     if (plainObject) {
-        data = key;
+      data = key;
     } else {
-        data[key] = value;
+      data[key] = value;
     }
     this.sessionCache = Object.assign(this.sessionCache || {}, data);
+  },
+
+  delSession(key) {
+    delete this.sessionCache[key];
+  }
+
 };
 
-context.delSession = function(key) {
-    delete this.sessionCache[key];
-};
+util.completeAssign(context, proto);
--- src/lib/util.js
+++ src/lib/util.js
@@ -5,58 +5,77 @@ const isFunction = require('lodash/isFunction');
 const isPlainObject = require('lodash/isPlainObject');
 
 exports.parseRequest = (ctx) => {
-    const requestPath = ctx.path;
-    const method = ctx.method;
-    let requestDesc = {
-        method: method,
-        path: requestPath,
-        finalPath: ctx.path,
-    };
-    if (/.json$/.test(requestPath)) {
-        let pathArr = requestPath.slice(0, -5).split('/').slice(1);
-        pathArr = remove(pathArr, (item) => {
-            return item !== '';
-        });
-        if (pathArr.length === 1) {
-            requestDesc.file = 'index.js';
-            requestDesc.funcName = camelCase(`${method} ${pathArr[0]} json`);
-        } else if (pathArr.length >= 2) {
-            requestDesc.file = pathArr.slice(0, -1).join('/') + '.js';
-            requestDesc.funcName = camelCase(`${method} ${pathArr.slice(-1)} json`);
-        }
-    } else if (/.html$/.test(requestPath)) {
-        let pathArr = requestPath.replace('.html', '').split('/').slice(1);
-        pathArr = remove(pathArr, (item) => {
-            return item !== '';
-        });
-        if (pathArr.length === 1) {
-            requestDesc.file = 'index.js';
-            requestDesc.funcName = camelCase(`${method} ${pathArr[0]} html`);
-        } else if (pathArr.length >= 2) {
-            requestDesc.file = pathArr.slice(0, -1).join('/') + '.js';
-            requestDesc.funcName = camelCase(`${method} ${pathArr.slice(-1)} html`);
-        }
-    } else {
-        let pathArr = requestPath.split('/').slice(1);
-        pathArr = remove(pathArr, (item) => {
-            return item !== '';
-        });
-        if (requestPath === '/' && method === 'GET') {
-            requestDesc.file = 'index.js';
-            requestDesc.funcName = 'getIndexHtml';
-        } else if (pathArr.length === 1) {
-            requestDesc.file = 'index.js';
-            requestDesc.funcName = camelCase(`${method} ${pathArr[0]} html`);
-        } else if (pathArr.length === 2) {
-            requestDesc.file = pathArr.join('/') + '.js';
-            requestDesc.funcName = 'getIndexHtml';
-            // temp
-            requestDesc.finalPath = path.resolve(requestPath, 'index');
-        } else if (pathArr.length >= 2) {
-            requestDesc.file = pathArr.slice(0, -1).join('/') + '.js';
-            requestDesc.funcName = camelCase(`${method} ${pathArr.slice(-1)} html`);
-        }
+  const requestPath = ctx.path;
+  const method = ctx.method;
+  let requestDesc = {
+    method: method,
+    path: requestPath,
+    finalPath: ctx.path,
+  };
+  if (/.json$/.test(requestPath)) {
+    let pathArr = requestPath.slice(0, -5).split('/').slice(1);
+    pathArr = remove(pathArr, (item) => {
+      return item !== '';
+    });
+    if (pathArr.length === 1) {
+      requestDesc.file = 'index.js';
+      requestDesc.funcName = camelCase(`${method} ${pathArr[0]} json`);
+    } else if (pathArr.length >= 2) {
+      requestDesc.file = pathArr.slice(0, -1).join('/') + '.js';
+      requestDesc.funcName = camelCase(`${method} ${pathArr.slice(-1)} json`);
     }
+  } else if (/.html$/.test(requestPath)) {
+    let pathArr = requestPath.replace('.html', '').split('/').slice(1);
+    pathArr = remove(pathArr, (item) => {
+      return item !== '';
+    });
+    if (pathArr.length === 1) {
+      requestDesc.file = 'index.js';
+      requestDesc.funcName = camelCase(`${method} ${pathArr[0]} html`);
+    } else if (pathArr.length >= 2) {
+      requestDesc.file = pathArr.slice(0, -1).join('/') + '.js';
+      requestDesc.funcName = camelCase(`${method} ${pathArr.slice(-1)} html`);
+    }
+  } else {
+    let pathArr = requestPath.split('/').slice(1);
+    pathArr = remove(pathArr, (item) => {
+      return item !== '';
+    });
+    if (requestPath === '/' && method === 'GET') {
+      requestDesc.file = 'index.js';
+      requestDesc.funcName = 'getIndexHtml';
+    } else if (pathArr.length === 1) {
+      requestDesc.file = 'index.js';
+      requestDesc.funcName = camelCase(`${method} ${pathArr[0]} html`);
+    } else if (pathArr.length === 2) {
+      requestDesc.file = pathArr.join('/') + '.js';
+      requestDesc.funcName = 'getIndexHtml';
+      // temp
+      requestDesc.finalPath = path.resolve(requestPath, 'index');
+    } else if (pathArr.length >= 2) {
+      requestDesc.file = pathArr.slice(0, -1).join('/') + '.js';
+      requestDesc.funcName = camelCase(`${method} ${pathArr.slice(-1)} html`);
+    }
+  }
 
-    return requestDesc;
+  return requestDesc;
 };
+
+exports.completeAssign = (target, ...sources) => {
+  sources.forEach((source = {}) => {
+    let descriptors = Object.keys(source).reduce((descriptors, key) => {
+      descriptors[key] = Object.getOwnPropertyDescriptor(source, key);
+      return descriptors;
+    }, {});
+
+    // Object.assign 默认也会拷贝可枚举的Symbols
+    Object.getOwnPropertySymbols(source).forEach(sym => {
+      let descriptor = Object.getOwnPropertyDescriptor(source, sym);
+      if (descriptor.enumerable) {
+        descriptors[sym] = descriptor;
+      }
+    });
+    Object.defineProperties(target, descriptors);
+  });
+  return target;
+}
